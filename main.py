bc9bd1472eb2d2c05f6225db63595d8c0e3fd2aa
Malbrett <79618243+Malbrett@users.noreply.github.com>
2021-03-29 20:04:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+class User:
+    def __init__(self, userid):
+        self.userid = userid
+
+    @classmethod
+    def newUser(cls, file):
+        while True:
+            newname = input("Please enter a 4 character userID: ")
+            newname.lower()
+            if len(newname) == 4:
+                file.write(newname+"/n")
+                break
+            else:
+                print("That userID is not 4 characters./n")
+        print("---")
+        while True:
+            newpass = hash(input("Please enter a new secure password: "))
+            confirmpass = hash(input("Please re-enter your secure password: "))
+            if newpass == confirmpass:
+                file.write(str(newpass)+"/n")
+                break
+            else:
+                print("That password does not match./n")
+        print("---")
+
+
+firstTime = False
+try:
+    open("userInfo", "x")
+    user = open("userInfo", "a")
+    firstTime = True
+except FileExistsError:
+    user = open("userInfo")
+finally:
+    if firstTime:
+        user = open("userInfo", "a")
+        User.newUser(user)
